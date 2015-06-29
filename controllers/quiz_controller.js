855b4bcb487a7df65be2cc4e7dda64c19d3fd2b0
salvadrian <safu77@gmail.com>
2015-06-29 18:14:13 +0200

    Modificación estilo

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,13 @@
 // GET /quizzes/question
 exports.question = function(req, res) {
-  res.render("quizzes/question", {pregunta: "Capital de Italia"});
+  res.render("quizzes/question", {pregunta: "Capital de Italia", title: 'Quiz | Preguntas'});
 };
 
 // GET /quizzes/answer
 exports.answer = function(req, res) {
-  if (req.query.respuesta === "Roma"){
-    res.render("quizzes/answer", { respuesta: "Correcto" });
+  if (req.query.respuesta.trim().toLowerCase() === "roma"){
+    res.render("quizzes/answer", { clase: "bien", respuesta: "Correcto", btnTit: "Volver", title: 'Quiz | Respuesta' });
   } else {
-    res.render("quizzes/answer", { respuesta: "Incorrecto" });
+    res.render("quizzes/answer", { clase: "mal", respuesta: "Incorrecto", btnTit: "Inténtelo otra vez", title: 'Quiz | Respuesta'});
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz | Inicio' });
 });
 
 router.get('/quizzes/question', quizController.question);
@@ -13,7 +13,7 @@ router.get('/quizzes/answer', quizController.answer);
 
 /* GET author page. */
 router.get('/author', function(req, res, next) {
-  res.render('author');
+  res.render('author', { title: 'Quiz | Créditos'});
 });
 
 
