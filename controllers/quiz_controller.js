d40ad2b76030cc839d152176fe506ae6a646f437
salvadrian <safu77@gmail.com>
2015-07-02 11:54:36 +0200

    (07) Modelo y base de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,19 @@
+var models = require('../models/models.js');
+
 // GET /quizzes/question
 exports.question = function(req, res) {
-  res.render("quizzes/question", {pregunta: "Capital de Italia", title: 'Quiz | Preguntas'});
+  models.Quiz.findAll().then(function(quiz) {
+    res.render("quizzes/question", {pregunta: quiz[0].pregunta, title: 'Quiz | Preguntas'});
+  });
 };
 
 // GET /quizzes/answer
 exports.answer = function(req, res) {
-  if (req.query.respuesta.trim().toLowerCase() === "roma"){
-    res.render("quizzes/answer", { clase: "bien", respuesta: "Correcto", btnTit: "Volver", title: 'Quiz | Respuesta' });
-  } else {
-    res.render("quizzes/answer", { clase: "mal", respuesta: "Incorrecto", btnTit: "Inténtelo otra vez", title: 'Quiz | Respuesta'});
-  }
+  models.Quiz.findAll().then(function(quiz) {
+    if (req.query.respuesta.trim().toLowerCase() === quiz[0].respuesta.toLowerCase()){
+      res.render("quizzes/answer", { clase: "bien", respuesta: "Correcto", btnTit: "Volver", title: 'Quiz | Respuesta' });
+    } else {
+      res.render("quizzes/answer", { clase: "mal", respuesta: "Incorrecto", btnTit: "Inténtelo otra vez", title: 'Quiz | Respuesta'});
+    }
+  });
 };
