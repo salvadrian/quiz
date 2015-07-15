ca705f27fb809b2a7b58be5c582eda817c45101d
salvadrian <safu77@gmail.com>
2015-07-15 18:19:22 +0200

    (16) Añadir Autenticación y sesión

---
+++ controllers/session_controller.js
@@ -0,0 +1,36 @@
+// GET /login   -- Formulario de login
+exports.new = function(req, res) {
+  var errors = req.session.errors || {};
+  req.session.errors = {};
+
+  res.render('sessions/new', { title: "Quiz | Nueva sesión", errors: errors });
+};
+
+// POST /login  -- Crear la sesión
+exports.create = function(req, res) {
+  console.log("ESTOY DENTRO DE CREATE");
+  var login = req.body.login;
+  var password = req.body.password;
+
+  var userController = require('./user_controller');
+  userController.autenticar(login, password, function(error, user) {
+    if (error) { // Si hay error devolvemos mensajes de error de sesión
+      req.session.errors = [{ "message": 'Se ha producido un error: ' + error }];
+      res.redirect("/login");
+      return;
+    }
+
+    // Crear req.session.user y guardar los campos: id y username
+    // La sesión se define por la existencia de: req.session.user
+    req.session.user = { id: user.id, username: user.username };
+
+    // Redirección a path anterior a login
+    res.redirect(req.session.redir.toString());
+  });
+};
+
+// DELETE /login  -- Destruir la sesión
+exports.destroy = function(req, res) {
+  delete req.session.user;
+  res.redirect(req.session.redir.toString()); // redirect a path anterior a login
+};
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
-
-// Autoload :quizId
-router.param('quizId', quizController.load);
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz', errors:[] });
 });
+
+// Autoload :quizId
+router.param('quizId', quizController.load);
+
+// Definición de rutas de session
+router.get('/login', sessionController.new); // Formulario login
+router.post('/login', sessionController.create); // Crear sesión
+router.delete('/login', sessionController.destroy); // Destruir sesión
 
 // Definición de rutas de /quizzes
 router.get('/quizzes', quizController.index);
@@ -22,6 +28,7 @@ router.get('/quizzes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizzes/:quizId(\\d+)', quizController.update);
 router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
 
+// Definición de rutas de comments
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', commentController.create);
 
